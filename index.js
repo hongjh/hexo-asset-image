3f1b38d88216379271f392eb7f57b0b4459222b6
Hong JianHong <hongjianhong@gmail.com>
2018-05-27 15:00:38 +0800

    Update index.js
    更新图片路径生成规则, permalink: posts/:year:month:day-:title.html

--- index.js
+++ index.js
@@ -1,62 +1,69 @@
 'use strict';
 var cheerio = require('cheerio');
 
+
 /**
  * 获得字符串中的第n次出现位置
- *
  * @see http://stackoverflow.com/questions/14480345/how-to-get-the-nth-occurrence-in-a-string
  */
 function getPosition(str, m, i) {
-  return str.split(m, i).join(m).length;
+    return str.split(m, i).join(m).length;
 }
 
-hexo.extend.filter.register('after_post_render', function(data){
-  var config = hexo.config;
-  if(config.post_asset_folder){
-    var link = data.permalink;
-	// 获取第3次出现 / 字符位置
-	var beginPos = getPosition(link, '/', 3) + 1;
-	// In hexo 3.1.1, the permalink of "about" page is like ".../about/index.html".
-	var endPos = link.lastIndexOf('/') + 1;
-    link = link.substring(beginPos, endPos);
+hexo.extend.filter.register('after_post_render', function(data) {
+    var config = hexo.config;
+    if (config.post_asset_folder) {
+        var link = data.permalink;
+        console.info("permalink:-->" + link);
+        // 获取第3次出现 / 字符位置
+        var beginPos = getPosition(link, '/', 3) + 1;
 
-    var toprocess = ['excerpt', 'more', 'content'];
-    for(var i = 0; i < toprocess.length; i++){
-      var key = toprocess[i];
- 
-      var $ = cheerio.load(data[key], {
-        ignoreWhitespace: false,
-        xmlMode: false,
-        lowerCaseTags: false,
-        decodeEntities: false
-      });
+        // In hexo 3.1.1, the permalink of "about" page is like ".../about/index.html".
+        //var endPos = link.lastIndexOf('/') + 1;
+        // permalink: posts/:year:month:day-:title.html
+        var endPos = link.lastIndexOf('.html');
 
-      $('img').each(function(){
-		if ($(this).attr('src')){
-			// For windows style path, we replace '\' to '/'.
-			var src = $(this).attr('src').replace('\\', '/');
-			if(!/http[s]*.*|\/\/.*/.test(src) &&
-			   !/^\s*\//.test(src)) {
-			  // For "about" page, the first part of "src" can't be removed.
-			  // In addition, to support multi-level local directory.
-			  var linkArray = link.split('/').filter(function(elem){
-				return elem != '';
-			  });
-			  var srcArray = src.split('/').filter(function(elem){
-				return elem != '' && elem != '.';
-			  });
-			  if(srcArray.length > 1)
-				srcArray.shift();
-			  src = srcArray.join('/');
-			  $(this).attr('src', config.root + link + src);
-			  console.info&&console.info("update link as:-->"+config.root + link + src);
-			}
-		}else{
-			console.info&&console.info("no src attr, skipped...");
-			console.info&&console.info($(this));
-		}
-      });
-      data[key] = $.html();
+        link = link.substring(beginPos, endPos) + '/';
+        var toprocess = ['excerpt', 'more', 'content'];
+        for (var i = 0; i < toprocess.length; i++) {
+            var key = toprocess[i];
+
+            var $ = cheerio.load(data[key], {
+                ignoreWhitespace: false,
+                xmlMode: false,
+                lowerCaseTags: false,
+                decodeEntities: false
+            });
+
+            $('img').each(function() {
+                if ($(this).attr('src')) {
+                    // For windows style path, we replace '\' to '/'.
+                    var src = $(this).attr('src').replace('\\', '/');
+
+                    console.info("src1:-->" + src);
+                    if (!/http[s]*.*|\/\/.*/.test(src) &&
+                        !/^\s*\//.test(src)) {
+                        // For "about" page, the first part of "src" can't be removed.
+                        // In addition, to support multi-level local directory.
+                        var linkArray = link.split('/').filter(function(elem) {
+                            return elem != '';
+                        });
+                        var srcArray = src.split('/').filter(function(elem) {
+                            return elem != '' && elem != '.';
+                        });
+                        if (srcArray.length > 1)
+                            srcArray.shift(); // 移除文章title部分
+                        src = srcArray.join('/');
+
+                        $(this).attr('src', config.root + link + src);
+                        console.info && console.info("update link as:-->" + config.root + link + src);
+                    }
+                } else {
+                    console.info && console.info("no src attr, skipped...");
+                    console.info && console.info($(this));
+                }
+            });
+            data[key] = $.html();
+        }
     }
-  }
 });
